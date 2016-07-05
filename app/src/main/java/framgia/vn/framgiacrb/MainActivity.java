a8c934e5526981e557ea4360fb07a09a8ee17067
luckyluke1994 <mddien1994@gmail.com>
2016-07-05 15:01:45 +0700

    Navigation Bar

--- app/src/main/java/framgia/vn/framgiacrb/MainActivity.java
+++ app/src/main/java/framgia/vn/framgiacrb/MainActivity.java
@@ -1,6 +1,11 @@
 package framgia.vn.framgiacrb;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
@@ -11,16 +16,33 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.SubMenu;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Locale;
+
+import framgia.vn.framgiacrb.fragment.CalendarFragment;
+import framgia.vn.framgiacrb.ui.CustomMonthCalendarView;
 
 public class MainActivity extends AppCompatActivity {
     private static final String CURRENT_MENU_ITEM = "currentMenuItem";
+    public static final String ACTION_BROADCAST = "DAY_CLICKED";
 
     private DrawerLayout mDrawerLayout;
     private NavigationView mNavigationView;
     private ActionBarDrawerToggle mDrawerToggle;
+    private AppBarLayout mAppBarLayout;
+    private CustomMonthCalendarView mCustomMonthCalendarView;
+    private TextView mDatePickerTextView;
+    private ImageView mArrow;
+    private dayClicked mDayClicked;
+    private RelativeLayout mDatePickerButton;
 
     int currentMenuItemId;
     boolean isExpanded = false;
@@ -29,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
     SubMenu subMenu;
     Toolbar toolbar;
 
-    SimpleDateFormat dateFormat = new SimpleDateFormat("d MMMM yyyy", /*Locale.getDefault()*/Locale.ENGLISH);
+    public static final SimpleDateFormat dateFormat = new SimpleDateFormat("d MMMM yyyy", /*Locale.getDefault()*/Locale.ENGLISH);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,14 +60,36 @@ public class MainActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         initUi();
+        assignHandler();
         updateDisplayView(R.id.day);
         currentMenuItemId = R.id.day;
     } // end of method onCreate
 
-    @SuppressWarnings("deprecation")
     private void initUi() {
+        mDatePickerTextView = (TextView) findViewById(R.id.date_picker_text_view);
+        mArrow = (ImageView) findViewById(R.id.date_picker_arrow);
+        mAppBarLayout = (AppBarLayout) findViewById(R.id.app_bar_layout);
+        mCustomMonthCalendarView = (CustomMonthCalendarView) findViewById(R.id.calendar_view);
+        mCustomMonthCalendarView.setAdapter(getSupportFragmentManager());
+        mDatePickerButton = (RelativeLayout) findViewById(R.id.date_picker_button);
+        Calendar calendar = Calendar.getInstance();
+        setSubTitle(dateFormat.format(calendar.getTime()));
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
         mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
+    } // end of method initUi()
+
+    @SuppressWarnings("deprecation")
+    private void assignHandler() {
+        mDatePickerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isExpanded) {
+                    closeToolbar();
+                } else {
+                    openToolbar();
+                }
+            }
+        });
         mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem item) {
@@ -77,18 +121,16 @@ public class MainActivity extends AppCompatActivity {
                 mDrawerToggle.syncState();
             }
         });
-    } // end of method initUi()
+    }
 
     private void updateDisplayView(int id) {
         Fragment fragment;
         switch (id) {
             case R.id.day:
                 break;
-            case R.id.few_day:
-                break;
             case R.id.month:
                 break;
-            case R.id.week:
+            case R.id.search:
                 break;
             default:
         }
@@ -129,6 +171,45 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void openToolbar() {
+        RotateAnimation anim = new RotateAnimation(currentRotation, currentRotation - 180.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        currentRotation = (currentRotation - 180.0f) % 360.0f;
+        anim.setInterpolator(new LinearInterpolator());
+        anim.setFillAfter(true);
+        anim.setFillEnabled(true);
+        anim.setDuration(300);
+        mArrow.startAnimation(anim);
+        mAppBarLayout.setExpanded(true, true);
+        isExpanded = true;
+    }
+
+    private void closeToolbar() {
+        RotateAnimation anim = new RotateAnimation(currentRotation, currentRotation + 180.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        currentRotation = (currentRotation + 180.0f) % 360.0f;
+        anim.setInterpolator(new LinearInterpolator());
+        anim.setFillAfter(true);
+        anim.setFillEnabled(true);
+        anim.setDuration(300);
+        mArrow.startAnimation(anim);
+        mAppBarLayout.setExpanded(false, true);
+        isExpanded = false;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mDayClicked = new dayClicked();
+        IntentFilter intentFilter = new IntentFilter(ACTION_BROADCAST);
+        registerReceiver(mDayClicked, intentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mDayClicked);
+        mDayClicked = null;
+    }
+
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         outState.putInt(CURRENT_MENU_ITEM, currentMenuItemId);
@@ -141,5 +222,21 @@ public class MainActivity extends AppCompatActivity {
         reCheckMenuItem(mNavigationView);
         updateDisplayView(currentMenuItemId);
         super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    private void setSubTitle(String date) {
+        if (null != mDatePickerTextView) {
+            mDatePickerTextView.setText(date);
+        }
+    }
+
+    private class dayClicked extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(ACTION_BROADCAST)) {
+                setSubTitle(intent.getStringExtra(CalendarFragment.TITLE));
+                closeToolbar();
+            }
+        }
     }
 }
---
+++ app/src/main/java/framgia/vn/framgiacrb/adapter/CalendarPagerAdapter.java
@@ -0,0 +1,39 @@
+package framgia.vn.framgiacrb.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import framgia.vn.framgiacrb.fragment.CalendarFragment;
+
+/**
+ * Created by lucky_luke on 7/5/2016.
+ */
+public class CalendarPagerAdapter extends FragmentPagerAdapter{
+    private static final int MIN_YEAR = 1970;
+    private static final int MAX_YEAR = 2050;
+
+    public CalendarPagerAdapter(FragmentManager fragmentManager) {
+        super(fragmentManager);
+    }
+
+    // Returns total number of pages
+    @Override
+    public int getCount() {
+        return (MAX_YEAR - MIN_YEAR) * 12;
+    }
+
+    // Returns the fragment to display for that page
+    @Override
+    public Fragment getItem(int position) {
+        int year = (position / 12) + MIN_YEAR;
+        int month = position % 12;
+        return CalendarFragment.newInstance(year, month);
+    }
+
+    // Returns the page title for the top indicator
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return "Page " + position;
+    }
+}
